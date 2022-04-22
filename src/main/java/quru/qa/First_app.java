918d7fe6c108971732dfcfb33ccc779ce2809699
dddpr <ddd.pronin@yandex.ru>
2022-04-22 16:55:45 +0300

    First app

---
+++ src/main/java/quru/qa/First_app.java
@@ -0,0 +1,50 @@
+package quru.qa;
+
+public class First_app {
+
+    public static void main(String[] args) {
+
+    int a = 6;
+    int b = 3;
+
+    System.out.println("a = " + a + "; b = " + b);
+    System.out.println("Сложение " + (a + b));
+    System.out.println("Вычитание " + (a - b));
+    System.out.println("Деление " + (a / b));
+    System.out.println("Умножение " + (a * b));
+    System.out.println("Остатка от деления " + (a % b));
+    System.out.println();
+
+    if ( a > b ) {
+        System.out.println("a больше b");
+    }
+    if ( a >= b ) {
+            System.out.println("a больше b");
+        }
+    if ( a < b ) {
+            System.out.println("a меньше b");
+        }
+    if ( a <= b ) {
+            System.out.println("a меньше b");
+        }
+    if ( a == b) {
+        System.out.println("а равно b");
+    }
+    if ( a != b) {
+        System.out.println("а не равно b");
+    }
+    System.out.println();
+
+    int num1 = Integer.MAX_VALUE + 1;
+    System.out.println(num1);
+    int num2 = Integer.MIN_VALUE - 1;
+    System.out.println(num2);
+    System.out.println();
+
+    int num3 = Byte.MAX_VALUE + 1;
+    System.out.println(num3);
+    int num4 = Byte.MIN_VALUE - 1;
+    System.out.println(num4);
+    System.out.println();
+    }
+}
